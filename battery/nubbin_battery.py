abe3b6767fcc652fed1a21f332decc8ba05c0779
CharitraP <charitrapyalimadannanavar@gmail.com>
2023-09-09 17:56:00 -0400

    Changes on battery files

--- battery/nubbin_battery.py
+++ battery/nubbin_battery.py
@@ -1,5 +1,4 @@
 from battery.battery import Battery
-from utils import add_years_to_date
 
 class NubbinBattery(Battery):
     def __init__(self, current_date, last_service_date):
@@ -7,7 +6,7 @@ class NubbinBattery(Battery):
         self.last_service_date = last_service_date
 
     def needs_service(self):
-        date_which_battery_should_be_serviced_by = add_years_to_date(self.last_service_date, 4)
+        date_which_battery_should_be_serviced_by = self.last_service_date+timedelta(days=365*4)
         if date_which_battery_should_be_serviced_by <= self.current_date:
             return True
         else:
--- battery/spindler_battery.py
+++ battery/spindler_battery.py
@@ -1,6 +1,4 @@
 from battery.battery import Battery
-from utils import add_years_to_date
-
 
 class SpindlerBattery(Battery):
     def __init__(self, current_date, last_service_date):
@@ -8,7 +6,7 @@ class SpindlerBattery(Battery):
         self.last_service_date = last_service_date
 
     def needs_service(self):
-        date_which_battery_should_be_serviced_by = add_years_to_date(self.last_service_date, 2)
+        date_which_battery_should_be_serviced_by = self.last_service_date+timedelta(days=365*4)
         if date_which_battery_should_be_serviced_by <= self.current_date:
             return True
         else:
